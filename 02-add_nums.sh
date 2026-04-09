f577505b160fa2b78f4583d59ed074e943597041
Danny Zhang <dzhang1@ggc.edu>
2026-04-08 23:07:55 -0400

    Add area calculation in parallel branch

--- 02-add_nums.sh
+++ 02-add_nums.sh
@@ -11,5 +11,14 @@ sum=$(( first+second+third ))
 
 echo "The sum is $sum"
 
-# exercise: ask the user for the width and height and present total
-# number of pixels
+# exercise: ask the user for the width and height and present total number of pixels
+
+echo "Enter width"
+read width
+
+echo "Enter height"
+read height
+
+area=$((width * height))
+
+echo "Area is: $area"
